145bdcb05589dbeee0ffdf8f25af6c2bc0c3cc8e
Thay <thays.santos@idwall.co>
2025-08-07 08:18:52 -0300

    feat: using web workers to improve performance

--- scripts.js
+++ scripts.js
@@ -1,148 +1,65 @@
-let fps,
-  duration,
-  populationSeed,
-  rule1,
-  rule2,
-  rule4,
-  cellSize,
-  rows,
-  cols,
-  animation;
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas").transferControlToOffscreen();
+const worker = new Worker("worker.js");
 const restartBtn = document.getElementById("restart");
 const rules = document.getElementById("rules");
-const ctx = canvas.getContext("2d");
-const backgroundColor = "teal";
-const cellColor = "#57fcc5ff";
 const timer = document.getElementById("time");
 
-const rulesHeight = rules.clientHeight;
-const width = window.innerWidth;
-const height = window.innerHeight - rulesHeight;
-
-const getNeighbors = (index, cells) => {
-  const neighborsIndexes = [
-    index - cols - 1, // up left
-    index - cols, // up
-    index - cols + 1, // up right
-    index - 1, // left
-    index + 1, // right
-    index + cols - 1, // bottom left
-    index + cols, // bottom
-    index + cols + 1, // bottom right
-  ];
-
-  const validNeighbors = neighborsIndexes.filter(
-    (x) => x >= 0 && x < cells.length
-  );
-  return validNeighbors.map((x) => cells[x]);
-};
-
-function updateCells(cells) {
-  const newCells = Array(cells.length);
-  for (let i = 0; i < cells.length; i++) {
-    const currentCell = cells[i];
-    const neighbors = getNeighbors(i, cells);
-    const aliveNeighbors = neighbors.filter((x) => x > 0).length;
-    if (currentCell === 1) {
-      if (aliveNeighbors < rule1) {
-        newCells[i] = 0;
-        continue;
-      }
-
-      if (aliveNeighbors <= rule2) {
-        newCells[i] = 1;
-        continue;
-      }
-
-      newCells[i] = 0;
-    } else {
-      if (aliveNeighbors === rule4) {
-        newCells[i] = 1;
-      } else {
-        newCells[i] = 0;
-      }
-    }
-  }
-
-  return newCells;
-}
-
-function draw(cells) {
-  ctx.clearRect(0, 0, width, height);
-
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-      if (cells[row * cols + col] === 1) {
-        ctx.fillStyle = cellColor;
-        ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
-      }
-    }
-  }
-}
-
-function seed() {
-  return Array.from({ length: rows * cols }, () =>
-    Math.random() >= populationSeed ? 1 : 0
-  );
-}
-
-function update(cells) {
-  const updatedCells = updateCells(cells);
-  draw(updatedCells);
-  return updatedCells;
-}
-
 function main() {
-  cancelAnimationFrame(animation);
-  animation = null;
-  rule1 = Number(document.getElementById("rule1").value);
-  rule2 = Number(document.getElementById("rule2").value);
-  rule4 = Number(document.getElementById("rule4").value);
-  fps = Number(document.getElementById("fps").value);
-  duration = Number(document.getElementById("duration").value) * 1000;
-  populationSeed = Number(document.getElementById("populationSeed").value);
-  cellSize = Number(document.getElementById("cellSize").value ?? 5);
-  cols = Math.floor(width / cellSize);
-  rows = Math.floor(height / cellSize);
+  params = getParams();
 
-  canvas.width = width;
-  canvas.height = height;
-
-  let cells = seed();
-  let start,
-    elapsed,
-    totalElapsed = 0,
-    lastFrame = Date.now();
-
-  const animate = (time) => {
-    if (!start) start = time;
-    totalElapsed = time - start;
-    elapsed = Date.now() - lastFrame;
-
-    if (elapsed > 1000 / fps) {
-      cells = update(cells);
-      lastFrame = Date.now();
-    }
-
-    timer.textContent = Math.floor(totalElapsed / 1000);
-
-    if (totalElapsed < duration) {
-      animation = requestAnimationFrame(animate);
-    } else {
-      cancelAnimationFrame(animation);
-    }
-  };
-
-  animation = requestAnimationFrame(animate);
+  worker.postMessage({
+    params,
+    type: "reload",
+  });
 }
 
-main();
+function getParams() {
+  const width = window.innerWidth;
+  const height = window.innerHeight - rules.clientHeight;
 
-restartBtn.addEventListener("click", main);
+  const getValueById = (id) => Number(document.getElementById(id).value ?? 0);
+
+  const cellSize = getValueById("cellSize") ?? 5;
+
+  return {
+    rule1: getValueById("rule1"),
+    rule2: getValueById("rule2"),
+    rule4: getValueById("rule4"),
+    fps: getValueById("fps"),
+    populationSeed: getValueById("populationSeed"),
+    cellColor: "#57fcc5ff",
+    cellSize,
+    cols: Math.floor(width / cellSize),
+    rows: Math.floor(height / cellSize),
+    width,
+    height,
+  };
+}
 
 document.querySelectorAll("input").forEach((input) => {
   input.addEventListener("input", (x) => {
     input.nextSibling.textContent = x.target.value;
+
+    worker.postMessage({
+      params: getParams(),
+      type: "update",
+    });
   });
 });
+
+worker.postMessage({ canvas, type: "init" }, [canvas]);
+main();
+
+restartBtn.addEventListener("click", main);
+
+window.addEventListener("resize", main);
+
+worker.onmessage = (event) => {
+  if (event.data.type === "status") {
+    timer.textContent = `${event.data.timer} / ${event.data.fps}fps`;
+  }
+
+  if (event.data.type === "end") {
+    timer.textContent = "end";
+  }
+};
---
+++ worker.js
@@ -0,0 +1,135 @@
+let canvas, ctx, animation;
+let params = {};
+
+function getNeighbors(index, cells) {
+  const neighborsIndexes = [
+    index - params.cols - 1, // up left
+    index - params.cols, // up
+    index - params.cols + 1, // up right
+    index - 1, // left
+    index + 1, // right
+    index + params.cols - 1, // bottom left
+    index + params.cols, // bottom
+    index + params.cols + 1, // bottom right
+  ];
+
+  const validNeighbors = neighborsIndexes.filter(
+    (x) => x >= 0 && x < cells.length
+  );
+  return validNeighbors.map((x) => cells[x]);
+}
+
+function updateCells(cells) {
+  const newCells = Array(cells.length);
+  for (let i = 0; i < cells.length; i++) {
+    const currentCell = cells[i];
+    const neighbors = getNeighbors(i, cells);
+    const aliveNeighbors = neighbors.filter((x) => x > 0).length;
+    if (currentCell === 1) {
+      if (aliveNeighbors < params.rule1) {
+        newCells[i] = 0;
+        continue;
+      }
+
+      if (aliveNeighbors <= params.rule2) {
+        newCells[i] = 1;
+        continue;
+      }
+
+      newCells[i] = 0;
+    } else {
+      if (aliveNeighbors === params.rule4) {
+        newCells[i] = 1;
+      } else {
+        newCells[i] = 0;
+      }
+    }
+  }
+
+  return newCells;
+}
+
+function draw(cells) {
+  ctx.clearRect(0, 0, params.width, params.height);
+
+  for (let row = 0; row < params.rows; row++) {
+    for (let col = 0; col < params.cols; col++) {
+      if (cells[row * params.cols + col] === 1) {
+        ctx.fillStyle = params.cellColor;
+        ctx.fillRect(
+          col * params.cellSize,
+          row * params.cellSize,
+          params.cellSize,
+          params.cellSize
+        );
+      }
+    }
+  }
+}
+
+function seed() {
+  console.log("seed", params.rows * params.cols);
+  return Array.from({ length: params.rows * params.cols }, () =>
+    Math.random() >= params.populationSeed ? 1 : 0
+  );
+}
+
+function update(cells) {
+  const updatedCells = updateCells(cells);
+  draw(updatedCells);
+  return updatedCells;
+}
+
+function getAnimation() {
+  cancelAnimationFrame(animation);
+  animation = null;
+  let cells = seed();
+  let start,
+    elapsed,
+    totalElapsed = 0,
+    lastFrame = Date.now();
+  elapsedFrames = 0;
+
+  const animate = (time) => {
+    if (!start) start = time;
+    totalElapsed = time - start;
+    elapsed = Date.now() - lastFrame;
+    elapsedFrames++;
+
+    if (elapsed > 1000 / params.fps) {
+      this.postMessage({
+        timer: Math.floor(totalElapsed / 1000),
+        fps: Math.max(1, Math.floor(1000 / elapsed)),
+        type: "status",
+      });
+      cells = update(cells);
+      elapsedFrames = 0;
+      lastFrame = Date.now();
+    }
+
+    animation = requestAnimationFrame(animate);
+  };
+
+  animation = requestAnimationFrame(animate);
+}
+
+onmessage = (event) => {
+  if (event.data.type === "init") {
+    canvas = event.data.canvas;
+    ctx = canvas.getContext("2d");
+    return;
+  }
+
+  if (event.data.type === "reload") {
+    params = event.data.params;
+    canvas.width = params.width;
+    canvas.height = params.height;
+    getAnimation();
+    return;
+  }
+
+  if (event.data.type === "update") {
+    console.log("update", event.data.params);
+    params = event.data.params;
+  }
+};
